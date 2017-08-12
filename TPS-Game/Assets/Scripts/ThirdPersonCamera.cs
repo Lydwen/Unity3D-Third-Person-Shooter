9900a1679043342b422f67322c16070f0286e260
Lydwen <tomlau94@aol.com>
2017-08-12 19:01:36 +0200

    Add a camera that put itself behind the player
    V1 of a TPS camera

---
+++ TPS-Game/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+    //need to follow a player
+    [SerializeField]
+    [Tooltip("Add the gameobject that the camera will follow in a TPS style")]
+    private Transform objectToFollowTransform;
+    [SerializeField]
+    [Tooltip("The space that the camera will have with the  gameobject")]
+    private float spaceBehindObject;
+    [SerializeField]
+    private Transform cameraTransform;
+
+    /// <summary>
+    ///     At each update, the camera will place itself behind the player
+    /// </summary>
+    private void Update()
+    {
+        Vector3 objectToFollowPosition = objectToFollowTransform.position;
+        Vector3 objectToFollowDirection = objectToFollowTransform.forward;
+        Quaternion objectToFollowRotation = objectToFollowTransform.rotation;
+
+        //Put the camera behind the player
+        cameraTransform.position = objectToFollowPosition - (objectToFollowDirection * spaceBehindObject);
+        //The camera film the player
+        cameraTransform.rotation = objectToFollowRotation;
+    }
+}
